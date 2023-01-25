72f057363819fffa09a0803ce172b953d82771b8
Terje Io <terjeio@users.noreply.github.com>
2023-01-25 21:25:23 +0100

    Updated for settings version change.

--- grblHAL_MKRZERO/src/driver.c
+++ grblHAL_MKRZERO/src/driver.c
@@ -4,7 +4,7 @@
 
   Part of grblHAL
 
-  Copyright (c) 2018-2022 Terje Io
+  Copyright (c) 2018-2023 Terje Io
 
   Grbl is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
@@ -862,7 +862,7 @@ static bool driver_setup (settings_t *settings)
 
  // Set defaults
 
-    IOInitDone = settings->version == 21;
+    IOInitDone = settings->version == 22;
 
     hal.settings_changed(settings);
 
@@ -985,7 +985,7 @@ bool driver_init (void) {
     IRQRegister(SysTick_IRQn, SysTick_IRQHandler);
 
     hal.info = "SAMD21";
-    hal.driver_version = "221022";
+    hal.driver_version = "230125";
     hal.driver_url = GRBL_URL "/SAMD21";
 #ifdef BOARD_NAME
     hal.board = BOARD_NAME;
